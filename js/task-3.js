ae20ccfd5dbb9eabb268945ba1e6ee1a571e0a80
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-05-12 14:44:22 +0200

    task-2 and task-3 update

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  let setNum = [];
+  for (const element of numbers) {
+    if (element > value) {
+      setNum.push(element);
+    }
+  }
+  return setNum;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
